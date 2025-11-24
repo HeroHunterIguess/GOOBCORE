de2115bdcc12504afcbcc218d8590ba4c74722c3
Raylee <abramscarter1999@gmail.com>
2025-11-24 08:09:14 -0500

    Update score if shield kills enemy

--- scripts/passives/shield.gd
+++ scripts/passives/shield.gd
@@ -3,5 +3,6 @@ extends Node2D
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Enemy"):
 		area.get_parent().takeDamage(99999)
+		Globals.overallScore += 1
 		queue_free()
 		Globals.hasShield = false
