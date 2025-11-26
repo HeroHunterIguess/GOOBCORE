5666789e1e6a494e538349b1b899bbac5fb0cf7e
HeroHunter <abramscarter1999@gmail.com>
2025-11-26 15:41:08 -0500

    finish spitter enemy

--- scripts/spit.gd
+++ scripts/spit.gd
@@ -1,25 +1,17 @@
 extends Node2D
 
-var health = 8
-var damage = 4
+var health = 1
+var damage = 1
 
 
 var explodeScene = preload("res://scenes/objects/deathexplosion.tscn")
-var spitPreload = preload("res://scenes/objects/spit.tscn")
+
 
 
 # take an amount of damage for the projectiles to call
 func takeDamage(amount):
 	health -= amount
-	# play hurt animation
-	$AnimatedSprite2D.visible = false
-	$hurtanim.visible = true
-	await get_tree().create_timer(0.15).timeout
-	$AnimatedSprite2D.visible = true
-	$hurtanim.visible = false
-	
-	# play hurt sfx
-	$deathSound.play()
+
 
 # take knockback from being hit by bigger attacks
 func takeKB(amount):
@@ -29,7 +21,7 @@ func takeKB(amount):
 # enemy will travel towards the player location
 func moveToPlayer(delta):
 	var speed = 105
-	var to_player = Globals.playerLocation - position
+	var to_player = Globals.playerLocation - self.global_position
 	var distance = to_player.length()
 	
 	if distance > 25:
@@ -45,23 +37,12 @@ func moveToPlayer(delta):
 func _process(delta):
 	
 	if health <= 0:
-		Globals.orbs += 2
 		Globals.overallScore += 1
 		#spawn and play death animation
 		var explosion = explodeScene.instantiate()
 		get_tree().get_current_scene().add_child(explosion)
-		explosion.global_position = self.position
-		# hide sprites
-		#await get_tree().create_timer(0.1).timeout
-		$AnimatedSprite2D.visible=false
-		$hurtanim.visible=false
+		explosion.global_position = self.global_position
 		# let death animation play out
 		queue_free()
 	else:
 		moveToPlayer(delta)
-
-
-func _on_spit_timer_timeout() -> void:
-	var spit = spitPreload.instantiate()
-	add_child(spit)
-	spit.global_position = Vector2(self.global_position)
